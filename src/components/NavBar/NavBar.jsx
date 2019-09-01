72a76c5666990ebe5d8d19a7010bf1debb7b3c6a
Vladimir Petrov <dizel852@gmail.com>
2019-09-01 20:50:43 +0300

    Add bootstrap and update navBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import * as style from './NavBar.module.scss';
+import { Nav, Navbar } from 'react-bootstrap';
 
 export const NavBar = () => (
   <React.Fragment>
-    <div className={style.navBarContainer}>
-      <NavLink className={style.navItem} to="/home">Home</NavLink>
-      <NavLink className={style.navItem} to="/about">About</NavLink>
-    </div>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand>Test app</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link as={NavLink} to="/home">
+          Home
+        </Nav.Link>
+        <Nav.Link as={NavLink} to="/about">
+          About
+        </Nav.Link>
+      </Nav>
+    </Navbar>
   </React.Fragment>
 );
